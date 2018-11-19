bda126c54c78a651ef2d4e2f298323c7b6b24cc7
Alexander Henze <alexander.henze@maibornwolff.de>
2018-11-19 17:54:55 +0100

    add fluent assertion package, add example test

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using app.Domain;
+using FluentAssertions;
 using Xunit;
 
 namespace test
@@ -8,7 +12,11 @@ namespace test
         [Fact]
         public void Test1()
         {
+            Customer c = new Customer {Name="Horst-Günther"};
+            IEnumerable numbers = new[] { 1, 2, 3 };
 
+            c.Name.Should().StartWith("Horst");
+            numbers.Should().HaveCount(4, "because we thought we put four items in the collection");
         }
     }
 }
